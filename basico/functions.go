8de705852cd4aff4b3323dcf14d94a7de9c11598
Andres Aguilar <andresyoshimar@gmail.com>
2017-12-22 20:55:24 -0600

    agregando funciones con pase de parametros

--- basico/functions.go
+++ basico/functions.go
@@ -1,12 +1,16 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 var variable_global int32
+
+func saludar(nombre string) {
+	fmt.Println("Hola", nombre)
+}
 
 func main() {
 	variable_global = 50
 	fmt.Println(variable_global)
+
+	saludar("Andres")
 }
